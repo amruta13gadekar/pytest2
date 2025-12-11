f259369a64dda020478cb21b0bcb747a8f995aa7
amruta13gadekar <ammugadekar8@gmail.com>
2025-12-11 16:11:36 +0530

    save

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ if __name__=="__main__":
     name = "Alice"
     emp_id="E1001"
     department="IT"
-    salary="55000"
+    salary=55000
     print(employee_details (name,emp_id,department,salary))
 
 
