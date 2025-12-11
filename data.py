b114929cf56b4c06577d0d523ef5fa81cefacc76
amruta13gadekar <ammugadekar8@gmail.com>
2025-12-11 15:46:18 +0530

    save

--- data.py
+++ data.py
@@ -2,8 +2,8 @@ def employee_details(name,emp_id,department,salary):
     result=(
         f"Employee Name:{name}\n"
         f"Employee ID :{emp_id}\n"
-        f"Department :{Department}\n"
-        f"Salary" :{salary}
+        f"Department :{department}\n"
+        f"Salary :{salary}\n"
     )
     return result
 if __name__=="__main__":
@@ -11,7 +11,7 @@ if __name__=="__main__":
     emp_id="E1001"
     department="IT"
     salary="55000"
-    print(employee_details (name,emp_id,department,salary))
+    print(employee_details (name,emp_id))
 
 
 
